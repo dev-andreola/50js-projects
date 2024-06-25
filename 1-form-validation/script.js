a6cfbe7d009410fdfc320b001e29849126441814
vtAndreola <dev.andreola@gmail.com>
2024-06-25 15:50:00 -0300

    feat(form-validation): add custom error messages

--- 1-form-validation/script.js
+++ 1-form-validation/script.js
@@ -4,6 +4,13 @@ goBackButton.addEventListener("click", () => {
   window.location.replace("/");
 });
 
+const formInputs = document.querySelectorAll("[required]");
+
+formInputs.forEach((input) => {
+  input.addEventListener("blur", () => inputValidation(input));
+  input.addEventListener("invalid", (e) => e.preventDefault());
+});
+
 const errorTypes = [
   "valueMissing",
   "typeMismatch",
@@ -31,30 +38,21 @@ const errorMessages = {
   cpf: {
     valueMissing: "O campo de CPF não pode estar vazio.",
     patternMismatch: "Por favor, preencha um CPF válido.",
-    customError: "O CPF digitado não existe.",
+    customError: "Por favor, preencha um CPF válido.",
     tooShort: "O campo de CPF não tem caractéres suficientes.",
   },
-  aniversario: {
+  birthday: {
     valueMissing: "O campo de data de nascimento não pode estar vazio.",
     customError: "Você deve ser maior que 18 anos para se cadastrar.",
   },
-  termos: {
+  term: {
     valueMissing: "Você deve aceitar nossos termos antes de continuar.",
   },
 };
 
-const formInputs = document.querySelectorAll("[required]");
-
-formInputs.forEach((input) => {
-  input.addEventListener("blur", () => inputValidation(input));
-  input.addEventListener("invalid", (e) => e.preventDefault());
-});
-
 function validateCPF(input) {
   const cpf = input.value.replace(/\.|-/g, "");
-  console.log(cpf);
 
-  // VALIDAÇÃO DE NÚMEROS REPETIDOS
   const invalidCPF = [
     "00000000000",
     "11111111111",
@@ -68,14 +66,22 @@ function validateCPF(input) {
     "99999999999",
   ];
 
+  console.log(cpf);
+
+  console.log(`invalidCPF.includes(cpf) = ${invalidCPF.includes(cpf)}`);
+  console.log(`firstDigitValidation(cpf) = ${!firstDigitValidation(cpf)}`);
+  console.log(`secondDigitValidation(cpf) = ${!secondDigitValidation(cpf)}`);
+
   if (
     invalidCPF.includes(cpf) ||
-    firstDigitValidation(cpf) ||
-    secondDigitValidation(cpf)
+    !firstDigitValidation(cpf) ||
+    !secondDigitValidation(cpf)
   ) {
-    console.log("esse cpf nao existe");
+    input.setCustomValidity("Por favor, preencha um CPF válido!");
+    console.log("cpf invalido");
   } else {
-    console.log("esse cpf existe");
+    input.setCustomValidity("");
+    console.log("cpf existe");
   }
 }
 
@@ -83,8 +89,8 @@ function firstDigitValidation(cpf) {
   let soma = 0;
   let multiplicador = 10;
 
-  for (let i = 0; i < 9; i++) {
-    soma += cpf[i] * multiplicador;
+  for (let tamanho = 0; tamanho < 9; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
     multiplicador--;
   }
 
@@ -101,8 +107,8 @@ function secondDigitValidation(cpf) {
   let soma = 0;
   let multiplicador = 11;
 
-  for (let i = 0; i < 10; i++) {
-    soma += cpf[i] * multiplicador;
+  for (let tamanho = 0; tamanho < 10; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
     multiplicador--;
   }
 
@@ -124,15 +130,39 @@ function validateAge(input) {
     birthday.getUTCDate()
   );
 
-  console.log(actualDate >= majority);
+  if (!(actualDate >= majority)) {
+    input.setCustomValidity("O usuário não é maior de idade");
+  } else {
+    input.setCustomValidity("");
+  }
 }
 
 function inputValidation(input) {
+  let message = "";
+  input.setCustomValidity("");
   if (input.name == "cpf" && input.value.length >= 11) {
     validateCPF(input);
   }
   if (input.name == "birthday" && input.value != "");
   {
     validateAge(input);
+  }
+
+  console.log(input.validity);
+
+  errorTypes.forEach((error) => {
+    if (input.validity[error]) {
+      message = errorMessages[input.name][error];
+      console.log(message);
+    }
+  });
+
+  const errorMessageOutput = input.parentNode.querySelector(".error-message");
+  const inputValidator = input.checkValidity();
+
+  if (!inputValidator) {
+    errorMessageOutput.textContent = message;
+  } else {
+    errorMessageOutput.textContent = "";
   }
 }
